724e228cb35b7372fd33db686446cf014e305f51
Sid <sidchaudhary04@gmail.com>
2026-03-19 09:26:49 +0000

    polished the look and feel, added 404 page, fixed routes

--- exams.js
+++ exams.js
@@ -1,9 +1,9 @@
 window.EXAMS = [
-  { "subject": "7517",  "paper": "Paper 1", "month": 6,  "day": 10 },
-  { "subject": "7517",  "paper": "Paper 2", "month": 6,  "day": 17 },
-  { "subject": "7517",  "paper": "NEA",     "month": 3,  "day": 26 },
-  { "subject": "1CP2",  "paper": "Paper 1", "month": 5,  "day": 13 },
-  { "subject": "1CP2",  "paper": "Paper 2", "month": 5,  "day": 19 },
-  { "subject": "FM",    "paper": "Y544",    "month": 6,  "day": 16 },
-  { "subject": "SM",    "paper": "Y240/2",  "month": 6,  "day": 18 }
+  { subject: "7517", paper: "Paper 1", month: 6, day: 10 },
+  { subject: "7517", paper: "Paper 2", month: 6, day: 17 },
+  { subject: "7517", paper: "NEA",     month: 3, day: 26 },
+  { subject: "1CP2", paper: "Paper 1", month: 5, day: 13 },
+  { subject: "1CP2", paper: "Paper 2", month: 5, day: 19 },
+  { subject: "FM",   paper: "Y544",    month: 6, day: 16 },
+  { subject: "SM",   paper: "Y240/2",  month: 6, day: 18 },
 ];
